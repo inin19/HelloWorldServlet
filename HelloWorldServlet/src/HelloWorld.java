3e29c3b46ab377b83e5f94b8003da264063acbc8
Yingying Chen <cyy870119@gmail.com>
2014-02-25 21:01:04 -0500

    2/25

--- HelloWorldServlet/src/HelloWorld.java
+++ HelloWorldServlet/src/HelloWorld.java
@@ -1,6 +1,7 @@
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -12,11 +13,21 @@ import javax.servlet.http.HttpServletResponse;
 import com.microstrategy.utils.Tree;
 import com.microstrategy.web.objects.WebDSN;
 import com.microstrategy.web.objects.WebFolder;
+import com.microstrategy.web.objects.WebGridHeaders;
+import com.microstrategy.web.objects.WebGridRows;
+import com.microstrategy.web.objects.WebHeader;
+import com.microstrategy.web.objects.WebHeaders;
 import com.microstrategy.web.objects.WebIServerSession;
 import com.microstrategy.web.objects.WebObjectInfo;
 import com.microstrategy.web.objects.WebObjectSource;
 import com.microstrategy.web.objects.WebObjectsException;
 import com.microstrategy.web.objects.WebObjectsFactory;
+import com.microstrategy.web.objects.WebReportData;
+import com.microstrategy.web.objects.WebReportGrid;
+import com.microstrategy.web.objects.WebReportInstance;
+import com.microstrategy.web.objects.WebReportSource;
+import com.microstrategy.web.objects.WebRow;
+import com.microstrategy.web.objects.WebRowValue;
 import com.microstrategy.web.objects.WebSearch;
 import com.microstrategy.webapi.EnumDSSXMLObjectTypes;
 import com.microstrategy.webapi.EnumDSSXMLSearchDomain;
@@ -45,36 +56,82 @@ public class HelloWorld extends HttpServlet {
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("doGet");
+		
 		WebObjectSource lObjectSource = factory.getObjectSource();
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<p>doGet<p>");
+		
 		try {
-			WebObjectInfo lReportObjectInfo = lObjectSource.getObject("6033F89144BDDF80F1EF5280D19CECEA",EnumDSSXMLObjectTypes.DssXmlTypeReportDefinition,true);
+			WebObjectInfo reportObject = lObjectSource.getObject("256263D142248D56446F3A80AD100C06",EnumDSSXMLObjectTypes.DssXmlTypeReportDefinition,true);
 			
-			WebSearch lSearch = lObjectSource.getNewSearchObject();
-
+//			String a = lReportObjectInfo.getName();
+//			
+//			String b = serverSession.getWebVersion();
 			
-			lSearch.setDisplayName("*");
-            lSearch.setSearchFlags(lSearch.getSearchFlags()
-                  + EnumDSSXMLSearchFlags.DssXmlSearchUsedByOneOf
-                  + EnumDSSXMLSearchFlags.DssXmlSearchNameWildCard);
-            lSearch.setAsync(false);
-            lSearch.types().add(new Integer(
-                  EnumDSSXMLObjectTypes.DssXmlTypeAttribute));
-            lSearch.types().add(new Integer(
-                  EnumDSSXMLObjectTypes.DssXmlTypeMetric));
-            lSearch.setDomain(
-                  EnumDSSXMLSearchDomain.DssXmlSearchDomainProject);
-            lSearch.usedBy().add(lReportObjectInfo);
-            lSearch.submit();
+			
+			WebReportSource reportSource=factory.getReportSource();
+			WebReportInstance reportInstance=null;
+			
+			try{
+				reportInstance=reportSource.getNewInstance(reportObject.getID());
+			}catch (WebObjectsException ex){
+				
+			}
 
+			reportInstance.setAsync(false);
+			 
+			WebReportGrid reportGrid=null;
+			
+			try {
+				WebReportData reportData=reportInstance.getResults();
+				reportGrid=reportData.getWebReportGrid();
+			} catch (WebObjectsException ex){
+				
+			}
+			
+			
+			
+			WebGridRows gridRows=reportGrid.getGridRows();
+			WebGridHeaders columnHeaders=reportGrid.getColumnHeaders();
+			
+			for (int i=0;i<columnHeaders.size();i++) {
+			    for (int j=0;j<reportGrid.getRowTitles().size();j++) {
+		
+			    }
+			    WebHeaders headers=columnHeaders.get(i);
+			    for (int j=0;j<headers.size();j++) {
+			    	WebHeader header=headers.get(j);
+			    	out.println(header.getDisplayName());
+			    }
+			    
+			}
+			
+			
+			
+			for (int i=0;i<gridRows.size();i++) {
+	            WebRow row=gridRows.get(i);
+	            WebHeaders rowHeaders=row.getHeaderElements();
+	            for (int j=0;j<rowHeaders.size();j++) {
+	                WebHeader header=rowHeaders.get(j);
+	                
+	                out.print(header.getDisplayName()+"\t");
+	                
+	            }
+	            for (int j=0;j<row.size();j++) {
+	                WebRowValue value=row.get(j);
+	               
+	                out.print(value.getValue()+"\t");
+	            }
+	            
+	        }
+			
             
-            WebFolder lSearchResults = lSearch.getResults();
             
             
+			closeSession(serverSession);
             
-            
-            
-            System.out.println("");
 			
 		} catch (WebObjectsException e) {
 			// TODO Auto-generated catch block
@@ -92,7 +149,7 @@ public class HelloWorld extends HttpServlet {
 		  
 		  factory = WebObjectsFactory.getInstance();
 		  serverSession = factory.getIServerSession();
-		  serverSession.setServerName("in-PC");
+		  serverSession.setServerName("inin");
 		  serverSession.setServerPort(0);
 		  serverSession.setProjectName("MicroStrategy Tutorial"); //just a project to connect to, not the cache we're going to clear (see above for projects to clear)
 		  serverSession.setLogin("Administrator");
@@ -105,6 +162,17 @@ public class HelloWorld extends HttpServlet {
 		  }  
 	}
 	
+	
+	public static void closeSession(WebIServerSession serverSession) {
+        try {
+            serverSession.closeSession();
+            
+        } catch (WebObjectsException ex) {
+           
+            return;
+        }
+       
+    }
 
 		 
 

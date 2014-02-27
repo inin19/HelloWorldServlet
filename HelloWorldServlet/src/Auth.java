8fafdaca61368fc762a37656cfb19b032246e7a7
inin <cyy870119@gmail.com>
2014-02-27 08:06:16 -0500

    2/27

--- HelloWorldServlet/src/Auth.java
+++ HelloWorldServlet/src/Auth.java
@@ -1,7 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.glassware.ListableMemoryCredentialStore;
+
 
 public class Auth {
-	private static final String CLIENTSECRETS_LOCATION = "";
-	
+	public static ListableMemoryCredentialStore store = new ListableMemoryCredentialStore();
+	  public static final String GLASS_SCOPE = "https://www.googleapis.com/auth/glass.timeline "
+	      + "https://www.googleapis.com/auth/glass.location "
+	      + "https://www.googleapis.com/auth/userinfo.profile";
+	  
+	  
 	
 	
 }
